3557e7da810055e5918a3106fe849a45adc95da7
demetrisbakas <demetrisbakas@hotmail.com>
2019-12-29 21:12:54 +0200

    Starting from scratch, New deployed demo to azure, connected to luis cognitive services.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=activitycoachingbot-Web-Deploy.pubxml -p:Password=DnLSGuiHYBc7WfrADwJt9LZdAKYvCWomFYPml4FQafJh5P5n5ln2GzcftfRN
+
