1c9549ac3ceeb5db1a68a94e1712bafe04ffa9ac
demetrisbakas <demetrisbakas@hotmail.com>
2020-06-06 20:55:40 +0300

    Removed the welcome message and card to make the bot work better in channels

--- Bots/DialogAndWelcomeBot.cs
+++ Bots/DialogAndWelcomeBot.cs
@@ -29,9 +29,9 @@ namespace Microsoft.BotBuilderSamples.Bots
                 // To learn more about Adaptive Cards, see https://aka.ms/msbot-adaptivecards for more details.
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {
-                    var welcomeCard = CreateAdaptiveCardAttachment();
-                    var response = MessageFactory.Attachment(welcomeCard, ssml: "Welcome to Bot Framework!");
-                    await turnContext.SendActivityAsync(response, cancellationToken);
+                    //var welcomeCard = CreateAdaptiveCardAttachment();
+                    //var response = MessageFactory.Attachment(welcomeCard, ssml: "Welcome to Bot Framework!");
+                    //await turnContext.SendActivityAsync(response, cancellationToken);
                     await Dialog.RunAsync(turnContext, ConversationState.CreateProperty<DialogState>("DialogState"), cancellationToken);
                 }
             }
