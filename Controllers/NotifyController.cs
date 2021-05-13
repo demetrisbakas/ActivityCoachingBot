a89ed676e35b44f7b52a46fd32002f398c5c7a01
demetrisbakas <demetrisbakas@hotmail.com>
2021-05-13 17:18:28 +0300

    Trying to fix proactive messages in teams

--- Controllers/NotifyController.cs
+++ Controllers/NotifyController.cs
@@ -1,18 +1,93 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.Integration.AspNet.Core;
-using Microsoft.Bot.Schema;
-using Microsoft.BotBuilderSamples.Dialogs;
-using Microsoft.Extensions.Configuration;
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Bot.Builder;
+//using Microsoft.Bot.Builder.Integration.AspNet.Core;
+//using Microsoft.Bot.Schema;
+//using Microsoft.BotBuilderSamples.Dialogs;
+//using Microsoft.Extensions.Configuration;
+//using System;
+//using System.Collections.Concurrent;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Threading;
+//using System.Threading.Tasks;
+
+//namespace CoreBot.Controllers
+//{
+//    [Route("api/notify")]
+//    [ApiController]
+//    public class NotifyController : ControllerBase
+//    {
+//        private readonly IBotFrameworkHttpAdapter _adapter;
+//        private readonly string _appId;
+//        private readonly ConcurrentDictionary<string, ConversationReference> _conversationReferences;
+
+//        public NotifyController(IBotFrameworkHttpAdapter adapter, IConfiguration configuration, ConcurrentDictionary<string, ConversationReference> conversationReferences)
+//        {
+//            _adapter = adapter;
+//            _conversationReferences = conversationReferences;
+//            _appId = configuration["MicrosoftAppId"];
+
+//            // If the channel is the Emulator, and authentication is not in use,
+//            // the AppId will be null.  We generate a random AppId for this case only.
+//            // This is not required for production, since the AppId will have a value.
+//            if (string.IsNullOrEmpty(_appId))
+//            {
+//                _appId = Guid.NewGuid().ToString(); //if no AppId, use a random Guid
+//            }
+//        }
+
+//        public async Task<IActionResult> Get()
+//        {
+//            try
+//            {
+//                await ((BotAdapter)_adapter).ContinueConversationAsync(_appId, _conversationReferences.Values.FirstOrDefault(), BotCallback, default);
+//            }
+//            catch (Exception e)
+//            {
+//                Console.WriteLine(e);
+//            }
+
+//            // Let the caller know proactive messages have been sent
+//            return new ContentResult()
+//            {
+//                Content = "<html><body><h1>Proactive messages have been sent.</h1></body></html>",
+//                ContentType = "text/html",
+//                StatusCode = (int)HttpStatusCode.OK,
+//            };
+//        }
+
+//        private async Task BotCallback(ITurnContext turnContext, CancellationToken cancellationToken)
+//        {
+//            // If you encounter permission-related errors when sending this message, see
+//            // https://aka.ms/BotTrustServiceUrl
+//            // http://localhost:3978/api/notify
+//            //await turnContext.SendActivityAsync("proactive hello");
+//            if (PersonalDetailsDialog.PersonalDetails.Cluster != null)
+//            {
+//                var tipMessage = await MainDialog.Response.TipMessageAsync();
+//                await turnContext.SendActivityAsync(tipMessage);
+//            }
+//        }
+//    }
+//}
+
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
 
-namespace CoreBot.Controllers
+namespace ProactiveBot.Controllers
 {
     [Route("api/notify")]
     [ApiController]
@@ -26,11 +101,8 @@ namespace CoreBot.Controllers
         {
             _adapter = adapter;
             _conversationReferences = conversationReferences;
-            _appId = configuration["MicrosoftAppId"];
+            _appId = configuration["MicrosoftAppId"] ?? string.Empty;
 
-            // If the channel is the Emulator, and authentication is not in use,
-            // the AppId will be null.  We generate a random AppId for this case only.
-            // This is not required for production, since the AppId will have a value.
             if (string.IsNullOrEmpty(_appId))
             {
                 _appId = Guid.NewGuid().ToString(); //if no AppId, use a random Guid
@@ -39,13 +111,9 @@ namespace CoreBot.Controllers
 
         public async Task<IActionResult> Get()
         {
-            try
+            foreach (var conversationReference in _conversationReferences.Values)
             {
-                await ((BotAdapter)_adapter).ContinueConversationAsync(_appId, _conversationReferences.Values.FirstOrDefault(), BotCallback, default);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
+                await ((BotAdapter)_adapter).ContinueConversationAsync(_appId, conversationReference, BotCallback, default(CancellationToken));
             }
 
             // Let the caller know proactive messages have been sent
@@ -59,15 +127,7 @@ namespace CoreBot.Controllers
 
         private async Task BotCallback(ITurnContext turnContext, CancellationToken cancellationToken)
         {
-            // If you encounter permission-related errors when sending this message, see
-            // https://aka.ms/BotTrustServiceUrl
-            // http://localhost:3978/api/notify
-            //await turnContext.SendActivityAsync("proactive hello");
-            if (PersonalDetailsDialog.PersonalDetails.Cluster != null)
-            {
-                var tipMessage = await MainDialog.Response.TipMessageAsync();
-                await turnContext.SendActivityAsync(tipMessage);
-            }
+            await turnContext.SendActivityAsync("proactive hello");
         }
     }
 }
