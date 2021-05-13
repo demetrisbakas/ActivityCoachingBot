54a92849b39056b606fe5c3086f5b278ccc8524c
demetrisbakas <demetrisbakas@hotmail.com>
2021-05-13 20:23:19 +0300

    Force authentication

--- Startup.cs
+++ Startup.cs
@@ -91,6 +91,7 @@ namespace Microsoft.BotBuilderSamples
             app.UseDefaultFiles();
             app.UseStaticFiles();
             app.UseWebSockets();
+            app.UseAuthentication();
 
             app.UseMvc();
         }
