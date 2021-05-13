6b7666715b96d0eba290fbb4ed33db99784e0bf4
demetrisbakas <demetrisbakas@hotmail.com>
2021-05-13 20:11:07 +0300

    Remove the OnMembersAddedAsync changes

--- Bots/DialogAndWelcomeBot.cs
+++ Bots/DialogAndWelcomeBot.cs
@@ -24,19 +24,18 @@ namespace Microsoft.BotBuilderSamples.Bots
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            //foreach (var member in membersAdded)
-            //{
-            //    // Greet anyone that was not the target (recipient) of this message.
-            //    // To learn more about Adaptive Cards, see https://aka.ms/msbot-adaptivecards for more details.
-            //    if (member.Id != turnContext.Activity.Recipient.Id)
-            //    {
-            //        //var welcomeCard = CreateAdaptiveCardAttachment();
-            //        //var response = MessageFactory.Attachment(welcomeCard, ssml: "Welcome to Bot Framework!");
-            //        //await turnContext.SendActivityAsync(response, cancellationToken);
-            //        await Dialog.RunAsync(turnContext, ConversationState.CreateProperty<DialogState>("DialogState"), cancellationToken);
-            //    }
-            //}
-            await Dialog.RunAsync(turnContext, ConversationState.CreateProperty<DialogState>("DialogState"), cancellationToken);
+            foreach (var member in membersAdded)
+            {
+                // Greet anyone that was not the target (recipient) of this message.
+                // To learn more about Adaptive Cards, see https://aka.ms/msbot-adaptivecards for more details.
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    //var welcomeCard = CreateAdaptiveCardAttachment();
+                    //var response = MessageFactory.Attachment(welcomeCard, ssml: "Welcome to Bot Framework!");
+                    //await turnContext.SendActivityAsync(response, cancellationToken);
+                    await Dialog.RunAsync(turnContext, ConversationState.CreateProperty<DialogState>("DialogState"), cancellationToken);
+                }
+            }
         }
 
         // Load attachment from embedded resource.
